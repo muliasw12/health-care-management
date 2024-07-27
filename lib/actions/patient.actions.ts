3bc9ae2e6adfcee50f6ce89a367c4820c6dc2759
Mulia Sujiastuti Wahyono <muliasujiastuti@gmail.com>
2024-07-27 08:28:47 +0700

    add consent and privacy component

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -1,8 +1,9 @@
 "use server";
 
 import { ID, Query } from "node-appwrite"
-import { users } from "../appwrite.config"
+import { BUCKET_ID, DATABASE_ID, databases, ENDPOINT, PATIENT_COLLECTION_ID, PROJECT_ID, storage, users } from "../appwrite.config"
 import { parseStringify } from "../utils";
+import { InputFile } from "node-appwrite/file"
 
 export const createUser = async (user: CreateUserParams) => {
   try {
@@ -26,6 +27,36 @@ export const getUser = async (user_id: string) => {
     const user = await users.get(user_id);
 
     return parseStringify(user);
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const registerPatient = async ({ identification_document, ...patient }: RegisterUserParams) => {
+  try {
+    let file;
+
+    if (identification_document) {
+      const inputFile = InputFile.fromBuffer(
+        identification_document?.get('blobFile') as Blob,
+        identification_document?.get('fileName') as string
+      )
+
+      file = await storage.createFile(BUCKET_ID!, ID.unique(), inputFile)
+    }
+
+    const newPatient = await databases.createDocument(
+      DATABASE_ID!,
+      PATIENT_COLLECTION_ID!,
+      ID.unique(),
+      {
+        identification_document_id: file?.$id || null,
+        identification_document_url: `${ENDPOINT}/storage/buckets/${BUCKET_ID}/files/${file?.$id}/view?project=${PROJECT_ID}`,
+        ...patient
+      }
+    )
+
+    return parseStringify(newPatient)
   } catch (error) {
     console.log(error)
   }
